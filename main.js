e180803ff3c46a34f7b01fa24d429e4212258220
yassin samir <yassinsam3@gmail.com>
2023-02-03 23:59:12 +0200

    applying guard clauses

--- main.js
+++ main.js
@@ -1,52 +1,51 @@
 // @ts-nocheck
-let input = document.querySelector("input");
-let button = document.querySelector(".main > button");
-let maindiv = document.querySelector(".main");
+const input = document.querySelector("input");
+const AddTaskBtn = document.querySelector(".main > button");
+const mainDiv = document.querySelector(".main");
 let f = 1;
 if (localStorage.length) {
   let v = Object.entries(localStorage);
   for (let i = 0; i < v.length; i++) {
-    if (typeof v[i][1] == "string") {
-      let k = document.createElement("div");
-      k.classList.add("task");
-      k.append(v[i][1]);
-      let btn = document.createElement("button");
-      btn.append("Delete");
-      k.setAttribute("id", f);
-      btn.onclick = () => {
-        maindiv.removeChild(btn.parentNode);
-        console.log(`task${k.getAttribute("id")}`);
-        window.localStorage.removeItem(
-          `task${String(Number(k.getAttribute("id")) + 1)}`
-        );
-      };
-      k.append(btn);
-      maindiv.append(k);
-    } else break;
+    if (typeof v[i][1] !== "string") break;
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task");
+    taskDiv.append(v[i][1]);
+    const btn = document.createElement("button");
+    btn.append("Delete");
+    taskDiv.setAttribute("id", f);
+    btn.onclick = () => {
+      mainDiv.removeChild(btn.parentNode);
+      console.log(`task${taskDiv.getAttribute("id")}`);
+      window.localStorage.removeItem(
+        `task${String(Number(taskDiv.getAttribute("id")) + 1)}`
+      );
+    };
+    taskDiv.append(btn);
+    mainDiv.append(taskDiv);
   }
 }
-button.onclick = () => {
-  if (input.value) {
-    let newdiv = document.createElement("div");
-    newdiv.append(input.value);
-    f++;
-    newdiv.classList.add("task");
-    newdiv.setAttribute("id", f);
-    window.localStorage.setItem(
-      `task${newdiv.getAttribute("id")}`,
-      input.value
-    );
-    let btn = document.createElement("button");
-    btn.append("Delete");
-    btn.onclick = () => {
-      maindiv.removeChild(btn.parentNode);
-      localStorage.removeItem(`task${newdiv.getAttribute("id")}`);
-      console.log(`task${newdiv.getAttribute("id")}`);
-    };
-    newdiv.append(btn);
-    maindiv.append(newdiv);
-    input.value = "";
-  } else {
+AddTaskBtn.onclick = () => {
+  if (!input.value) {
     alert("error please try again");
+    return;
   }
+  const newTaskDiv = document.createElement("div");
+  newTaskDiv.append(input.value);
+  f++;
+  newTaskDiv.classList.add("task");
+  newTaskDiv.setAttribute("id", f);
+  window.localStorage.setItem(
+    `task${newTaskDiv.getAttribute("id")}`,
+    input.value
+  );
+  const btn = document.createElement("button");
+  btn.append("Delete");
+  btn.onclick = () => {
+    mainDiv.removeChild(btn.parentNode);
+    localStorage.removeItem(`task${newTaskDiv.getAttribute("id")}`);
+    console.log(`task${newTaskDiv.getAttribute("id")}`);
+  };
+  newTaskDiv.append(btn);
+  mainDiv.append(newTaskDiv);
+  input.value = "";
 };
